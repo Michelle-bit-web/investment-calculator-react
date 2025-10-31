a29e0b745de00d012d0f35aadb6c070a7c1b5fed
Michelle <mpuschkarow@web.de>
2025-10-31 17:51:38 +0100

    User Input state management added

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,23 +1,42 @@
+import { useState } from "react"
+
 export default function UserInput() {
+
+    const [userInput, saveUserInput] = useState({
+        initialInvestment: 1000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    })
+
+    function handleChange(inputID, newValue) {
+        saveUserInput(prevValues => {
+            return {
+                ...prevValues,
+                [inputID]: newValue
+            }
+        });
+    }
+
     return <section id="user-input">
         <div className="input-group">
             <p>
-                <label >Initial Investment</label>
-                <input type="number" required />
+                <label for="initialInvestment" >Initial Investment</label>
+                <input id="initialInvestment" type="number" required value={userInput} onChange={(event) => handleChange('initialInvestment', event.target.value)}/>
             </p>
             <p>
-                <label >Annual Investment</label>
-                <input type="number" required />
+                <label for="annualInvestment" >Annual Investment</label>
+                <input id="annualInvestment" type="number" required value={userInput} onChange={(event) => handleChange('annualInvestment', event.target.value)}/>
             </p>
         </div>
           <div className="numbert-group">
             <p>
-                <label >Expected Return</label>
-                <input type="number" required />
+                <label for="expectedReturn" >Expected Return</label>
+                <input id="expectedReturn" type="number" required value={userInput} onChange={(event) => handleChange('expectedReturn', event.target.value)}/>
             </p>
             <p>
-                <label >Duration</label>
-                <input type="number" required />
+                <label for="duration" >Duration</label>
+                <input id="duration" type="number" required value={userInput} onChange={(event) => handleChange('duration', event.target.value)}/>
             </p>
         </div>
 
